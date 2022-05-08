508e86ec590bc0d06414ae0c1e4f98d48b970356
Michael Waterfall <michaeljames.waterfall@gmail.com>
2022-05-08 15:17:49 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Coin from './components/coin/Coin';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="React Logo" className='App-logo' />
+        <h1 className='App-title'>
+          Coin Exchange     
+        </h1>
+      </header>
+      <table className='coin-table'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Ticker</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Coin name="Bitcoin" ticker="BTC" price={9999.99} />
+          <Coin name="Ethereum" ticker="ETC" price={299.99} />
+          <Coin name="Tether" ticker="USDT" price={1.0} />
+          <Coin name="Ripple" ticker="XRP" price={0.2} />
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
