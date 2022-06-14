e33a21eef8b20956bb07920406ddfb0323a1e133
Michael Waterfall <michaeljames.waterfall@gmail.com>
2022-06-14 05:25:26 +0100

    Coin test

--- src/components/CoinList/CoinList.jsx
+++ src/components/CoinList/CoinList.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Coin from '../Coin/Coin.jsx';
+import Coin from '../Coin/Coin';
 import styled from 'styled-components';
 import NewsList from '../News/NewsList.js';
 
